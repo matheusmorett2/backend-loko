8bba0b146900fbff7db72f166689edb547ccb10a
matheusmorett2 <matheusmorett300@gmail.com>
2022-10-05 00:42:44 -0300

    removing dist

--- dist/src/middlewares/exceptionMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exceptionMiddleware = void 0;
-const exceptionMiddleware = (err, _req, res, _next) => {
-    res.status(err.status).send(err.message);
-};
-exports.exceptionMiddleware = exceptionMiddleware;
